4b01aff20bd32b29fb73b1c1d6706d2e6400bd70
Daniel Caaz <dcaazy@gmail.com>
2023-01-02 16:37:28 -0800

    Download all the images! Mostly.

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 
 const fs = require('fs');
 const path = require('path');
+const sharp = require('sharp');
+const axios = require('axios');
 
 const SteamAPIClient = require('./SteamAPIClient');
 
 const config = require('./config.json');
 
 const client = new SteamAPIClient(config.steamApiKey);
+const workshopDir = path.join(__dirname, 'WorkshopItems');
+const userDir = path.join(__dirname, 'SteamUsers');
 
 const filePath = process.argv[2];
 
@@ -19,7 +23,6 @@ if (!filePath) {
 async function run(workshopId) {
     try {
         const workshopInfo = await client.getWorkshopInfo(workshopId);
-        const workshopDir = path.join(__dirname, 'WorkshopItems');
         if (!fs.existsSync(workshopDir)) {
             fs.mkdirSync(workshopDir);
         }
@@ -27,7 +30,6 @@ async function run(workshopId) {
 
         const userId = workshopInfo.creator;
         const userInfo = await client.getUserInfo(userId);
-        const userDir = path.join(__dirname, 'SteamUsers');
         if (!fs.existsSync(userDir)) {
             fs.mkdirSync(userDir);
         }
@@ -51,4 +53,41 @@ async function main() {
     }
 }
 
+async function extractAndConvertImages(workshopId) {
+    try {
+        const workshopInfo = require(path.join(workshopDir, `${workshopId}.json`));
+        const imageDir = path.join(workshopDir, workshopId);
+        if (!fs.existsSync(imageDir)) {
+            fs.mkdirSync(imageDir);
+        }
+        const imageUrls = workshopInfo.description.match(/\[img\](.+?)\[\/img\]/g);
+        if (imageUrls == null)
+            return;
+        for (const imageUrl of imageUrls) {
+            try {
+                const url = imageUrl.replace(/\[img\]|\[\/img\]/g, '');
+                const fileName = url.match(/\/([^/]+)$/)[1];
+                // Ensure that the file name ends in .png
+                const filePath = path.join(imageDir, fileName.endsWith('.png') ? fileName : `${fileName}.png`);
+                const response = await axios({
+                    method: 'get',
+                    url: url,
+                    responseType: 'arraybuffer'
+                });
+                await sharp(response.data).toFile(filePath);
+            } catch (error) {
+                console.error("Error downloading image");
+            }
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
 main();
+
+const workshopIds = fs.readdirSync(workshopDir).map(fileName => fileName.replace('.json', ''));
+for (const workshopId of workshopIds) {
+    extractAndConvertImages(workshopId);
+}
